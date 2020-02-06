2a089f0bdfb16335a8f750e93947591793b9ff22
Jacob Hunter <tank000715@gmail.com>
2020-02-05 23:37:31 -0600

    fix to keep the console window open when running the testApp.exe

--- testApp/testApp.cpp
+++ testApp/testApp.cpp
@@ -6,5 +6,9 @@ using namespace std;
 int main()
 {
     setKeyLength(26);
-    cout << getFibVal(15);
+    cout << getFibVal(15) << endl;
+    cout << "Enter a any character to close the program: ";
+
+    char c;
+    cin >> c;
 }
